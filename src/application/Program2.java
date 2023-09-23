6a068067cc883115e4e8b16fd6fffce5d8086b50
Tiago <tiagaoalb@gmail.com>
2023-09-23 08:16:12 -0300

    chore: add DepartmentDao files

---
+++ src/application/Program2.java
@@ -0,0 +1,39 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("===== TEST 1: department findById =====");
+        Department department = departmentDao.findById(3);
+        System.out.println(department);
+
+        System.out.println("\n===== TEST 3: department insert =====");
+        Department newDepartment = new Department();
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
+
+        System.out.println("\n===== TEST 4: department update =====");
+        department = departmentDao.findById(1);
+        department.setName("Xablau");
+        departmentDao.update(department);
+        System.out.println("Update completed!");
+
+        System.out.println("\n===== TEST 5: department delete =====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed!");
+
+        sc.close();
+    }
+}
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,45 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.sql.*;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private final Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
